c38fe5f350636c8a53d59feab9fea64d2de60da1
kartik <kartikprakash3775@gmail.com>
2020-04-04 13:07:34 -0500

    updated scale and visualization of accel_mag orientation rviz visualization

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SensorFusionWS
+
+### Visualize Phone Orientation (Just from Accelerometer and Magnetometer data)
+```
+$ roscore
+$ rosrun sensor_fusion_pkg sensor_streamer.py
+$ rosrun sensor_fusion_pkg test_node_for_accel_mag.py
+$ roslaunch phone_description phone_spawn.launch
+$ roslaunch phone_description phone_rviz.launch
+
+```
--- src/SensorFusion/src/test_node_for_accel_mag.py
+++ src/SensorFusion/src/test_node_for_accel_mag.py
@@ -7,13 +7,13 @@ from rospy import Time
 import numpy as np
 import math as m
 import message_filters # for time synchronization
-
 ## INITIALIZE THE NODE
 rospy.init_node("RPY_accel_mag_node")
 
 ## GLOBAL VARIABLES
 curtime = Time.now().secs + Time.now().nsecs * 10 ** (-9)
 orientation = []
+DEBUG = True
 
 ## CREATE A PUBLISHER FOR FUSED ORIENTATION
 fused_pub = rospy.Publisher("/Fused_orientation_topic", SensorMsgStamped, queue_size = 10)
@@ -26,6 +26,10 @@ def got_accel_mag(msg_accel, msg_mag):
     ax, ay, az = msg_accel.data
     mx, my, mz = msg_mag.data
 
+    if DEBUG:
+        print("raw acceleration values ax : {}, ay : {}, az : {}".format(ax, ay, az))
+        print("raw magnetometer values mx : {}, my : {}, mz : {}".format(mx, my, mz))
+
     ## NORMALIZE THE ACCELEROMETER DATA
     norm_accel =  m.sqrt(ax**2 + ay**2 + az**2)
     ax = ax / norm_accel
@@ -33,32 +37,41 @@ def got_accel_mag(msg_accel, msg_mag):
     az = az / norm_accel
 
     # ## NORMALIZE THE MAGNETOMETER DATA
-    # norm_mag =  m.sqrt(mx**2 + my**2 + mz**2)
-    # mx = mx / norm_mag
-    # my = my / norm_mag
-    # mz = mz / norm_mag
+    norm_mag =  m.sqrt(mx**2 + my**2 + mz**2)
+    mx = mx / norm_mag
+    my = my / norm_mag
+    mz = mz / norm_mag
+
+    if DEBUG:
+        print("normalized acceleration values ax : {}, ay : {}, az : {}".format(ax, ay, az))
+        print("normalized magnetometer values mx : {}, my : {}, mz : {}".format(mx, my, mz))
 
     ## GET THE ROLL AND PITCH
-    roll = m.atan2(ay, az)
-    pitch = m.atan2(-ax, m.sqrt(ay**2 + az**2))
+    rollA = m.atan2(ay, az)
+    pitchA = m.atan2(-ax, m.sqrt(ay**2 + az**2))
 
     ## COMPENSATE FOR YAW USING MAGNETOMETER
-    Mx = mx * m.cos(pitch) + mz * m.sin(pitch)
-    My = mx * m.sin(roll) * m.sin(pitch) + my * m.cos(roll) - mz * m.sin(roll) * m.cos(pitch)
+    Mx = mx * m.cos(pitchA) + mz * m.sin(pitchA)
+    My = mx * m.sin(rollA) * m.sin(pitchA) + my * m.cos(rollA) - mz * m.sin(rollA) * m.cos(pitchA)
 
-    yaw = m.atan2(-My, Mx)
+    yawM = m.atan2(-My, Mx)
+
+    if DEBUG:
+        print("rollA : {}, pitchA : {}, yawM : {}".format(m.degrees(rollA), m.degrees(pitchA), m.degrees(yawM)))
+        print("###############################################################################")
+        print("                                                                               ")
 
     ## CONVERT TO quaternion_from_euler
-    q = quaternion_from_euler(roll, pitch, yaw)
+    q = quaternion_from_euler(rollA, pitchA, yawM)
 
     ## LETS PUBLISH THIS TO THE BROADCASTER
-    tf_br.sendTransform((1,1,1), (q[0],q[1],q[2],q[3]), Time.now(), 'phone', 'world')
+    tf_br.sendTransform((0.0,0.0,0.5), (q[0],q[1],q[2],q[3]), Time.now(), 'base_link', 'world')
 
 if __name__ == "__main__":
     try:
         mag_sub = message_filters.Subscriber("/Magneto_topic_stamped", SensorMsgStamped)
         accel_sub = message_filters.Subscriber("/Accel_topic_stamped", SensorMsgStamped)
-        combined_sub = message_filters.ApproximateTimeSynchronizer([accel_sub, mag_sub], queue_size = 5, slop = 0.1)
+        combined_sub = message_filters.ApproximateTimeSynchronizer([accel_sub, mag_sub], queue_size = 5, slop = 0.05)
         combined_sub.registerCallback(got_accel_mag)
         rospy.spin()
     except rospy.ROSInterruptException:
@@ -72,6 +85,6 @@ if __name__ == "__main__":
 # STEPS:
 # 1. Get the raw accel values and the magnetometer values
 # 2. Try combining resultant accel and magnetometer to figure out RPY
-#
+# 3. Figure out why yaw values are not accurate.
 #
 ########################
